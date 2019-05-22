387c50cba81ecd7efd3387518347affbb3dd46eb
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-05-22 23:18:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  element= [ ]
-  array.each do |value|
-    element << value**2 
-  end
-  element 
-end
+# def square_array(array)
+#   element= [ ]
+#   array.each do |value|
+#     element << value**2 
+#   end
+#   element 
+# end
 
 
 def square_array(array)
